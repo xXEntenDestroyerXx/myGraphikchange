8016f304e0ea047377ec084c8c9df1bb1c77ef00
SirBadDev <patrick.tischendorf@web.de>
2020-09-05 12:59:42 +0200

    animation and new sprites added

--- public/client.js
+++ public/client.js
@@ -23,6 +23,30 @@ document.getElementById('post0').addEventListener('click', event => {
     fetchPost(0);
 })
 
+document.getElementById('post6').addEventListener('click', event => {
+    fetchPost(6);
+})
+
+document.getElementById('post8').addEventListener('click', event => {
+    fetchPost(8);
+})
+
+document.getElementById('post10').addEventListener('click', event => {
+    fetchPost(10);
+})
+
+document.getElementById('post11').addEventListener('click', event => {
+    fetchPost(11);
+})
+
+document.getElementById('post12').addEventListener('click', event => {
+    fetchPost(12);
+})
+
+document.getElementById('post13').addEventListener('click', event => {
+    fetchPost(13);
+})
+
 function fetchPost(message){
     const data = {message};
     const options = {
